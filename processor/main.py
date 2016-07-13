4808efe9d480571734003bb60c04614c1d078336
Gavin Rehkemper <gavreh@gmail.com>
2016-07-12 22:27:31 -0500

    distance calculation and updated db and csv

--- processor/main.py
+++ processor/main.py
@@ -2,7 +2,7 @@ import sqlite3
 from geo import findAddress
 from openpyxl import load_workbook
 from datetime import datetime
-
+from geopy.distance import vincenty
 
 
 # constants
@@ -51,13 +51,19 @@ def getInsertRow(conn, year, month, crimeIndex, streetNumber, streetName, howMan
 
 	streetNumber2 = None
 	streetNumber2LatLon = [None, None]
+	distance = None
 	if(len(splitAddress) > 1):
 		streetNumber2 = splitAddress[1]
 		streetNumber2LatLon = findAddress(conn, streetNumber2 + " " + streetName, "University City", "MO", "63130")
+		# distance:
+		streetNumber1LatLonTuple = (streetNumber1LatLon[0], streetNumber1LatLon[1])
+		streetNumber2LatLonTuple = (streetNumber2LatLon[0], streetNumber2LatLon[1])
+		distance = vincenty(streetNumber1LatLonTuple, streetNumber2LatLonTuple).miles
+		
 
 	retList = []
 	for i in list(xrange(howMany)):
-		retList.append((unix_time_millis(dt), crimeForIndex(crimeIndex), streetNumber1, streetNumber2, streetName, streetNumber1LatLon[0], streetNumber1LatLon[1], streetNumber2LatLon[0], streetNumber2LatLon[1]))
+		retList.append((unix_time_millis(dt), crimeForIndex(crimeIndex), streetNumber1, streetNumber2, streetName, streetNumber1LatLon[0], streetNumber1LatLon[1], streetNumber2LatLon[0], streetNumber2LatLon[1], distance))
 	return retList
 
 def addWorkbookData(fn):
@@ -76,7 +82,7 @@ def addWorkbookData(fn):
 						if(idx != 0 and idx != 1):
 							if (cell.value > 0):
 								listOfTuples = getInsertRow(conn, currentYear, currentMonth, idx, row[0].value, row[1].value, cell.value)
-								conn.executemany('INSERT INTO crimes VALUES (?,?,?,?,?,?,?,?,?,null)', listOfTuples)
+								conn.executemany('INSERT INTO crimes VALUES (?,?,?,?,?,?,?,?,?,?)', listOfTuples)
 	conn.commit()
 	conn.close()
 
